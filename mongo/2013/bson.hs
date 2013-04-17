048ba9dc6a284c597de140c50002b123a4157618
jerng <yangjerng@gmail.com>
2013-04-17 21:08:16 +0800

    Trashy Json/Bson query lexer/parser.

--- mongo/2013/bson.hs
+++ mongo/2013/bson.hs
@@ -3,33 +3,105 @@
 import Data.Bson
 import Data.Maybe
 import Data.Text (Text)
+import qualified Data.Text as T
 
 
-main :: IO (Maybe Value)
-main = return $ lookupBson (Doc doc)  ""
+main = return $ 
+  lexJSQ 
+  --".something.something[0][234].something"
+  --"something.something[0][234].something" -- missing [ or .
+  --"..something.something[0][234].something" -- empty string 
+  --".something.something.[0][234].something" -- empty string 
+
+  --".something.something[[0][234].something" -- extra [
+  --".something.something][0][234].something" -- extra ]
+  --".something.something[][234].something" -- missing integer
+  ".something.something[0a][234].something" -- extra non-integer 
+  --"].something.something[0][234].something" -- extra closer
+
+  -- after all that, we'll still have problems with special characters.
+
+type JavaScriptSyntaxAddress = Text 
+data BsonKey  = DocKey Text  -- document keys
+              | ArrayKey Int   -- array keys
+              deriving (Show)
+
+{- TESTS:
+
+legitimate addresses:
+  
+  .a                    maps to Just $ String "z"
+  .c.dsd.df             maps to Just $ Int32 74
+  .something[0].x       maps to Just $ String "sdfsdf"
+  .something[4][3]      maps to Just $ Float 234.22
+
+lexJSQ :: JavaScriptSyntaxAddress -> [BsonKey]
+
+-}
+
+lexJSQ :: Text -> [BsonKey]
+lexJSQ text = case text of 
+  ""  -> []
+  _   -> reverse $ notInToken text []
+
+notInToken :: Text -> [BsonKey] -> [BsonKey]
+notInToken text acc = case text of
+  ""        -> acc
+  otherwise ->  case T.splitAt 1 text of 
+    (".",rem) -> inTextToken rem acc
+    ("[",rem) -> inIntToken rem acc 
+    ("]",_) -> error "notInToken: unexpected ']'"
+    otherwise -> error $
+      "notInToken: expected a '.', '[', or ']' at the beginning of \"" 
+      ++ (T.unpack text)
+      ++ "\""
+
+
+inTextToken :: Text -> [BsonKey] -> [BsonKey]
+inTextToken text acc = case T.span (\x-> and [x/='.',x/='[']) text of
+  (token,rem) -> notInToken rem $ (DocKey token):acc
+
+inIntToken :: Text -> [BsonKey] -> [BsonKey]
+inIntToken text acc = case T.span (/=']') text of
+  (token,rem) -> expectSquareCloser rem $ 
+    (ArrayKey (read $ T.unpack $ token :: Int)):acc
+
+expectSquareCloser :: Text -> [BsonKey] -> [BsonKey]
+expectSquareCloser text acc = case T.splitAt 1 text of
+  ("]",rem) -> notInToken rem acc
+  otherwise -> error "expectSquareCloser: did not find a closeing ']'"
+
+
+
+
+
+
+
+
+--main :: IO (Maybe Value)
+--main = return $ lookupBson (Doc doc)  ""
 
 lookupBson :: Value -> JavaScriptSyntaxAddress -> Maybe Value
 lookupBson value jsq = Nothing
 
-type JavaScriptSyntaxAddress = Text 
 
-{-
 
-legitimate addresses:
-  
-  ['a']                 maps to Just $ String "z"
-  .a                    maps to Just $ String "z"
-  
-  ['c']['dsd']['df]     maps to Just $ Int32 74
-  .c.dsd.df             maps to Just $ Int32 74
-  
-  ['something'][0]['x'] maps to Just $ String "sdfsdf"
-  .something[0].x       maps to Just $ String "sdfsdf"
-  
-  ['something'][4][3]   maps to Just $ Float 234.22
-  .something[4][3]      maps to Just $ Float 234.22
 
--}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 doc :: Document 
 doc = ["a" := String "z", "b" := Int32 43, "c" := Doc subdoc, "something":=arrayarray]
