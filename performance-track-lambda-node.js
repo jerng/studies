789ad2578e565cf6b4b5942ad40880c7b8bae84e
jerng <yangjerng@gmail.com>
2020-05-12 13:42:56 +0800

    Cleaned up performance tracker.

--- performance-track-lambda-node.js
+++ performance-track-lambda-node.js
@@ -10,28 +10,27 @@
 /*  EXAMPLE OF USE: 
  *
    'use strict'
-    const L = require ( './libraries.js')
-    
+    const P = require ( './perf.js')
     
     exports.handler = async (event, context) =>  {
-        const startCPU = L.process.cpuUsage()
-        const startValues = {
-            startTime : L.performance.now(),
-            startCPUsum : startCPU.system + startCPU.user
-        }
         
-        L.mark ( `handler BEGAN`, startValues, true )
+        L.mark ( `handler BEGAN`, true )
             // FIRST CALL MUST SET THIRD PARAMETER TO TRUE
             
         const AWS = require ( 'aws-sdk' )
         
-        L.mark ( `AWS-SDK LOADED`, startValues )
+        L.mark ( `AWS-SDK LOADED` )
             // SUBSEQUENT CALLS MUST SET THIRD PARAMETER TO FALSY
+
+        // do something else
+        
+        L.mark ( `Did something else.`)
+        
+        return "a response"
+    }
 
 *
 */
-
-
 
 //
 //
@@ -39,72 +38,72 @@
 //
 
 let newExecutionContext = true
-let firstInvocation = true
 let nthInvocation
 let preInvocationTime
 let preInvocationCPUsum
 
-//  Measuring wallclock run time.
+    //
+    //
+    //
+    //
+    //  Measuring wallclock run time.
+    //      There exists a similar Web API
 const { performance } = require('perf_hooks')
-        // There exists a similar Web API
-const initTime = performance.now()
-        // This will only run once per execution context (EC);
-        // multiple function invocations may occur within the same EC;
-        // periodically, the Lambda service will destroy the EC;
-        // a function invocation after that must create a new EC. 
 let lastTime
+let invocationStartTime
 
-//
-//
-//
-//
-
-//  Measuring RAM usage.
-//      `node.process.memoryUsage` keys: rss, heapTotal, heapUsed, external
-//      AWS Lambda: Billed memory seems to include 35-40MB over ['rss'].
+    //
+    //
+    //
+    //
+    //  Measuring RAM usage.
+    //      `node.process.memoryUsage` keys: rss, heapTotal, heapUsed, external
+    //      AWS Lambda: Billed memory seems to include 35-40MB over ['rss'].
 const memoryUsageKey = 'rss'
 const padLength = 9
 let lastMem = process.memoryUsage()[memoryUsageKey]
     // There exists a similar Web API
 
-//
-//
-//
-//
-
-//  Measuring CPU time consumed (a subset of wallclock time).
-    // Using `process.cpuUsage`
-    // There exists NO similar Web API
-const initCPU = process.cpuUsage()
-        // This will only run once per execution context (EC);
-        // multiple function invocations may occur within the same EC;
-        // periodically, the Lambda service will destroy the EC;
-        // a function invocation after that must create a new EC. 
-const initCPUsum = initCPU.user + initCPU.system
+    //
+    //
+    //
+    //
+    //  Measuring CPU time consumed (a subset of wallclock time).
+    //      Using `process.cpuUsage`
+    //      There exists NO similar Web API
 let lastCPUsum
+let invocationStartCPUsum
 
-//
-//
-//
-//
-const mark = ( taskLabel, startValues, firstInHandler ) => {
+    //
+    //
+    //
+    //  Core functionality:
+    
+const mark = ( taskLabel, firstInHandler ) => {
 
     if ( newExecutionContext ) {
         if ( firstInHandler ) {
-            const preInvocationCPU = process.cpuUsage()
-            preInvocationCPUsum =   preInvocationCPU.user 
+            const preInvocationCPU  = process.cpuUsage()
+            preInvocationCPUsum     = preInvocationCPU.user 
                                     + preInvocationCPU.system
-            preInvocationTime = performance.now()
-            newExecutionContext = false
+            preInvocationTime       = performance.now()
+            
+            console.log (`⚠ perf.js : these figures are loose and fast; ⚠`)
+            console.log (`⚠ Lambda does not charge for preinvocation runtime; nodejs overhead seems to be 30MB; ⚠`)
+            console.log (
+                String('').padEnd ( 26 + 8 * padLength, `-` )
+            )
             console.log (
                 
-                String(`preInvoke: ` + Math.round(preInvocationTime) + ` ms`)
+                String(`preInvoke: `    + Math.round(preInvocationTime) 
+                                        + ` ms`)
                     .padStart ( 2 * padLength + 4, ` `) , 
 
                 String(`"${memoryUsageKey}"`)
                     .padStart ( 2 * padLength + 6, ` `) , 
                 
-                String(`preInvoke: ` + Math.round(preInvocationCPUsum/1000) + ` ms`)
+                String(`preInvoke: `    + Math.round(preInvocationCPUsum/1000) 
+                                        + ` ms`)
                     .padStart ( 2 * padLength + 4, ` `) , 
 
                 String(`throttle ⚠️ ㇏㇏`)
@@ -117,18 +116,17 @@ const mark = ( taskLabel, startValues, firstInHandler ) => {
         } else {
             throw   `perf.js; mark(_,_,firstInHandler); firstInHandler needs to 
                     be set to true, if this is the first time you are calling 
-                    (mark); you can view an example in the commetns of perf.js`
+                    (mark); you can view an example in the comments of perf.js`
         }
     } 
-    else 
-    if ( firstInvocation && firstInHandler ) { firstInvocation = false }
-        //  The second time the execution context runs into 
-        //  (firstInHandler == true), it tell itself this is no longer the 
-        //  first invocation; now we have a way to flag the first invocation 
-        //  for all calls to (mark).
 
     if ( firstInHandler ) {
-        nthInvocation = 0
+        nthInvocation               = 0
+        
+        invocationStartTime         = performance.now()
+        const invocationStartCPU    = process.cpuUsage()
+        invocationStartCPUsum       = invocationStartCPU.user 
+                                    + invocationStartCPU.system
         console.log (
             String(`WALL-🕓:(Δ,Σ)`)
                 .padStart ( 5 + 2 * padLength, ` ` ),
@@ -144,15 +142,6 @@ const mark = ( taskLabel, startValues, firstInHandler ) => {
         )
     }
     nthInvocation ++
-
-    if (    ( ! startValues ) 
-        ||  ( ! startValues.startTime )
-        ||  ( ! startValues.startCPUsum )
-    ) { 
-        throw   `perf.js; mark(_,startValues,_); startValues needs to have the 
-                (startTime) and (startCPUsum) values; you can view an example 
-                in the comments of perf.js`
-    }
 
     lastTime = firstInHandler 
         ? 0 
@@ -170,127 +159,132 @@ const mark = ( taskLabel, startValues, firstInHandler ) => {
     let tempMem
     
     console.log ( 
-        
-    //
-    //
-    //
-    //  (Block 1)
-    
-    //  dRUN : stage-to-stage difference in wallclock time in milliseconds;
-    //  tRUN : to-stage total wallclock time in milliseconds;
-
-    // delta of runtime;
-    Math.round (    dTime =
-                    (   tempTime = performance.now() 
-                        - ( firstInvocation ? 0 : startValues.startTime ) 
-                    ) - lastTime 
-                 
-    ).toString().padStart ( padLength, ` ` ), 
-
-    // total runtime;
-    Math.round ( lastTime = tempTime 
-    ).toString().padStart ( padLength, ` ` ), 
-
-    `ms|`,
-
-    //
-    //
-    //
-    //  (Block 2)
-
-    //  d : stage-to-stage difference in memory (whichever  metric you keyed);
-    //  t : to-stage total memory consumed for (whichever metric you keyed );
-    
-    // delta of RAM usage;
-    
-    (   Math.round (                // decimal point formatting;
-    
-            ( ( tempMem = process.memoryUsage()[memoryUsageKey] ) - lastMem )
             
-            / Math.pow( 1024, 2 )   // B to MB conversion;
-            * 100) / 100            // decimal point formatting;
-    )
-    .toString().padStart ( padLength, ` ` ), 
-
-    // total RAM usage;
+        //
+        //
+        //
+        //  (Block 1)
+        
+        //  dRUN : stage-to-stage difference in wallclock time in milliseconds;
+        //  tRUN : to-stage total wallclock time in milliseconds;
     
-    (   Math.round (                // decimal point formatting;
+        // delta of runtime;
+        Math.round (    dTime =
+                        (   tempTime    = performance.now() 
+                                        - invocationStartTime 
+                        ) - lastTime 
+                     
+        ).toString().padStart ( padLength, ` ` ), 
     
-            ( lastMem = tempMem )
-            / Math.pow ( 1024, 2 )  // B to MB conversion;:
-            * 100) / 100            // decimal point formatting;
-    ).toString().padStart ( padLength, ` ` ),
-    `MB|`, 
-   
-    //
-    //
-    //
-    //  (Block 3)
+        // total runtime;
+        Math.round ( lastTime = tempTime 
+        ).toString().padStart ( padLength, ` ` ), 
     
-    //  dCPU : stage-to-stage difference in CPU time consumed; 
-    //  tCPU : to-stage total CPU time consumed;
+        `ms|`,
     
-    // delta of CPU time consumed;
+        //
+        //
+        //
+        //  (Block 2)
     
-    Math.round (
-        ( dCPUsum = (   tempCPU = process.cpuUsage(), 
-                        tempCPUsum =    
-                            tempCPU.user 
-                            + tempCPU.system
-                            - ( firstInvocation ? 0 : startValues.startCPUsum )
+        //  d : stage-to-stage difference in memory (whichever metric you keyed);
+        //  t : to-stage total memory consumed for (whichever metric you keyed );
+        
+        // delta of RAM usage;
+        
+        (   Math.round (                // decimal point formatting;
+        
+                (   ( tempMem = process.memoryUsage()[memoryUsageKey] ) 
+                    - lastMem )
+                
+                / Math.pow( 1024, 2 )   // B to MB conversion;
+                * 100) / 100            // decimal point formatting;
+        )
+        .toString().padStart ( padLength, ` ` ), 
+    
+        // total RAM usage;
+        
+        (   Math.round (                // decimal point formatting;
+        
+                ( lastMem = tempMem )
+                / Math.pow ( 1024, 2 )  // B to MB conversion;:
+                * 100) / 100            // decimal point formatting;
+        ).toString().padStart ( padLength, ` ` ),
+        `MB|`, 
+       
+        //
+        //
+        //
+        //  (Block 3)
+        
+        //  dCPU : stage-to-stage difference in CPU time consumed; 
+        //  tCPU : to-stage total CPU time consumed;
+        
+        // delta of CPU time consumed;
+        
+        Math.round (
+            ( dCPUsum = (   tempCPU = process.cpuUsage(), 
+                            tempCPUsum =    
+                                tempCPU.user 
+                                + tempCPU.system
+                                - invocationStartCPUsum
+                        )
+                        - lastCPUsum 
                     )
-                    - lastCPUsum 
-                )
-                / 1000              // microsecond to millisecond conversion;
-    ).toString().padStart ( padLength, ` ` ),
+                    / 1000              // microsecond to millisecond conversion;
+        ).toString().padStart ( padLength, ` ` ),
+    
+        // total CPU time consumed;
+        
+        Math.round (
+            (   lastCPUsum  = tempCPUsum ) 
+            / 1000                      // microsecond to millisecond conversion;
+        ).toString().padStart( padLength, ` ` ), 
+        `ms|`, 
+    
+        //
+        //
+        //
+        //  (Block 4)
+    
+        //  ( delta of CPU time consumed / delta of runtime ); 
+        //  stage-to-stage CPU allocation; volatile; subject to long-term average;
+        
+        Math.round (
+            dCPUsum / dTime 
+        ).toString().padStart ( padLength, ` ` ), 
+        
+        //
+        //
+        //
+        //  (Block 5)
+    
+        //  ( total CPU time consumed / total runtime );
+        //  to-stage average CPU allocation; the long-term average;
+        //  AWS Lambda's fractional CPU allocation, based on max memory limit;
+        //
+        //  for example, a function allocated       128     ( MB of RAM ),
+        //      is correspondingly allocated        0.125   ( vCPUs ),
+        //                                      ==  80      ( us of CPU time, per 
+        //                                                    ms of runtime );
+        
+        Math.round (
+            lastCPUsum / lastTime 
+        ).toString().padStart ( padLength, ` ` ), 
+        `µs/ms|`,
+        
+        //
+        //
+        //
+        //
+        
+        taskLabel,
+        ( nthInvocation % 3  ?  "": "\n" )
+    )
 
-    // total CPU time consumed;
+    newExecutionContext = false
     
-    Math.round (
-        (   lastCPUsum  = tempCPUsum ) 
-        / 1000                      // microsecond to millisecond conversion;
-    ).toString().padStart( padLength, ` ` ), 
-    `ms|`, 
-
-    //
-    //
-    //
-    //  (Block 4)
-
-    //  ( delta of CPU time consumed / delta of runtime ); 
-    //  stage-to-stage CPU allocation; volatile; subject to long-term average;
-    
-    Math.round (
-        dCPUsum / dTime 
-    ).toString().padStart ( padLength, ` ` ), 
-    
-    //
-    //
-    //
-    //  (Block 5)
-
-    //  ( total CPU time consumed / total runtime );
-    //  to-stage average CPU allocation; the long-term average;
-    //  AWS Lambda's fractional CPU allocation, based on max memory limit;
-    //
-    //  for example, a function allocated       128     ( MB of RAM ),
-    //      is correspondingly allocated        0.125   ( vCPUs ),
-    //                                      ==  80      ( us of CPU time, per 
-    //                                                    ms of runtime );
-    
-    Math.round (
-        lastCPUsum / lastTime 
-    ).toString().padStart ( padLength, ` ` ), 
-    `µs/ms|`,
-    
-    //
-    //
-    //
-    //
-    
-    taskLabel,
-    ( nthInvocation % 3  ?  "": "\n" )
-) }
+}
 
 //  So long as Lambdas are kept warm, variables declared outside the following
 //      `module.exports` will not be reexecuted; only the body of the exported 
