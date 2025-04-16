fb8838efaa9283d5eb883d3674d1d0845e58096c
jerng <yangjerng@gmail.com>
2025-04-17 01:36:24 +0800

    Synced code

--- aws-lambda/os-only-runtime-sample-bash-scripts-modified/hello.sh
+++ aws-lambda/os-only-runtime-sample-bash-scripts-modified/hello.sh
@@ -1,8 +1,8 @@
 function handler () {
     EVENT_DATA=$1
 
-    #RESPONSE="{\"statusCode\": 200, \"body\": \"Hello from Lambda!\"}"
-    
+    # RESPONSE="{\"statusCode\": 200, \"body\": \"Hello from Lambda!\"}"
+    # 
     RESPONSE="The EVENT_DATA : $EVENT_DATA"
     
     echo $RESPONSE
