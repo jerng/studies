02b323d3ef3794f687cb237a1a57940f72c3f553
jerng <yangjerng@gmail.com>
2025-04-23 12:50:27 +0800

    deployed to production : big bulky garbage collected erlang POC

--- aws-lambda/os-only-erlang/README.md
+++ aws-lambda/os-only-erlang/README.md
@@ -1,14 +1,43 @@
-from [here](https://github.com/erlang/otp/blob/OTP-27.3.3/HOWTO/INSTALL.md)
+# working
+
 ```
+# Amazon Linux 2023 is based on (el9 / Red Hat Enterprise Linux 9)
+curl -L -o otp.rpm https://github.com/rabbitmq/erlang-rpm/releases/download/v27.3.3/erlang-27.3.3-1.el9.aarch64.rpm
+
+sudo dnf install -y otp.rpm
+vi main.erl
+
+zip -r otp.zip /usr/lib64/erlang/
+erlc mainb.erl 
+# download  otp.zip         -> /var/task/erlang
+#   and     mainb.beam      -> /var/task/mainb.beam
+#   via scp, then upload to -> lambda
+
+# on lambda : /var/task/bootstrap :
+#
+#   #!/bin/bash
+#   set -euo pipefail
+#   /var/task/erlang/bin/erl -noshell -s mainb loop -s init stop
+ 
+
+```
+
+# not working
+
+From [here](https://github.com/erlang/otp/blob/OTP-27.3.3/HOWTO/INSTALL.md)
+
+```
+sudo dnf install git g++ ncurses-devel openssl-devel perl
+# gcc will do, but g++ is needed for JIT
+
 git clone https://github.com/erlang/otp.git --branch=OTP-27.3.3 --single-branch --depth=1
 cd otp
-
 git checkout OTP-27.3.3
-sudo apt install libncurses-dev libssl-dev
+
 export ERL_TOP=`pwd` 
 export LANG=C
 ./configure
-make -j4
+make -j$(nproc)
 make -j4 release_tests
 cd release/tests/test_server
 
--- aws-lambda/os-only-erlang/mainb.erl
+++ aws-lambda/os-only-erlang/mainb.erl
@@ -16,12 +16,14 @@ not RuntimeIdCandidate ->
 true ->
     io:format(<<"~s~s">>,[RuntimeIdCandidate,<<"'\n">>]),
     { GetReturned, GetData } = httpc:request(get, 
-            %{ [<<"http://">>, RuntimeIdCandidate, <<"/2018-06-01/runtime/invocation/next">>],
-            {<<"http://localhost:8080">>,
+            { [<<"http://">>, RuntimeIdCandidate, <<"/2018-06-01/runtime/invocation/next">>],
+            %{<<"http://localhost:8080">>,
                 []},
             [],[]),
 
     case GetReturned of 
+    error -> 
+        io:format(standard_error,<<"~s">>,[<<"GET failed\n">>]);
     ok -> 
         io:format(<<"GET succeeded\n  InvocationId : ">>),
         % io:format("~p",[GetData]),
@@ -31,29 +33,30 @@ true ->
             element(2,GetData)),
         
         case InvocationIdCandidate of
-        {_,{_,InvocationId}} -> 
-            io:format(<<"~s~s~s">>, [ <<"'">>, InvocationId, <<"'\n">> ]);
         false ->
-            io:format(standard_error,<<"~s">>,[<<"NO HEADER\n">>])
-        end, 
-
-        { PostReturned, PostData } = httpc:request(post,
-                %{ [<<"http://">>, RuntimeIdCandidate,<<"/2018-06-01/runtime/invocation/">>, InvocationId,<<"/response">>],
-                {<<"http://localhost:8080">>,
-                    [],"",<<"">>},
-                [],[]
-        ),
-        case PostReturned of 
-        ok -> 
-            io:format(<<"POST succeeded\n  EVENT_DATA : ">>),
-            io:format(<<"~p">>,[element(3,PostData)]); 
-        error ->
-            io:format(standard_error,<<"~s">>,["POST failed\n"]) 
+            io:format(standard_error,<<"~s">>,[<<"NO HEADER\n">>]);
+        {_,{_,InvocationId}} -> 
+            io:format(<<"~s~s~s">>, [ <<"'">>, InvocationId, <<"'\n">> ]),
+        
+            EventData = element(3,GetData),
+            { PostReturned, _ } = httpc:request(post,
+                    { [<<"http://">>, RuntimeIdCandidate,<<"/2018-06-01/runtime/invocation/">>, InvocationId,<<"/response">>],
+                    %{<<"http://localhost:8080">>,
+                        [],
+                        %<<"Content-Type: text/plain">>,
+                        [<<"text/plain">>],
+                        EventData},
+                    [],[]
+            ),
+            case PostReturned of 
+            ok -> 
+            %    io:format(<<"POST succeeded\n  EVENT_DATA : ">>),
+            %    io:format(<<"~p">>,[EventData]); 
+                silence;
+            error ->
+                io:format(standard_error,<<"~s">>,["POST failed\n"]) 
+            end
         end
-        ;
-
-    error -> 
-        io:format(standard_error,<<"~s">>,[<<"GET failed\n">>])
     end
 end,
 loop()
