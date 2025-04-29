397812b7396ba8ee80e6aa56b8a5373f7a21acaa
jerng <yangjerng@gmail.com>
2025-04-30 00:49:28 +0800

    fortran : aws-lambda : prep

---
+++ aws-lambda/os-only-fortran/get-post/main.f90
@@ -0,0 +1,11 @@
+! GET : sample : https://github.com/interkosmos/fortran-curl/blob/master/examples/http/http.f90
+!
+! POST : sample : https://github.com/interkosmos/fortran-curl/blob/master/examples/post/post.f90
+!
+! Regex : sample : https://github.com/perazz/fortran-regex/tree/main
+
+program get_post 
+
+  ! This is a comment line; it is ignored by the compiler
+  print *, 'Hello, World!'
+end program get_post
