e76ea27af10fd3b64bb34218926470ea2bf31652
jerng <yangjerng@gmail.com>
2025-05-19 12:41:14 +0800

    sh : subshell expansion quoting : solved

--- _lang/sh/subshell-variable-expansion.sh
+++ _lang/sh/subshell-variable-expansion.sh
@@ -1,1 +1,6 @@
+# problems ( different )
 bash -xvc 'y="2> /dev/null";echo test0; z=$(echo test1; echo test2 "$y")'
+bash -xvc 'y="2> /dev/null";echo test0; z=$(echo test1; echo test2 $y)'
+
+# solution
+bash -xvc 'y="2> /dev/null";echo test0; z=$(echo test1; echo test2 '"$y"')'
