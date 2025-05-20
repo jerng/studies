502723beb81f436f6c4d658df4c7b3694f6b28b8
jerng <yangjerng@gmail.com>
2025-05-20 11:48:36 +0800

    carbon : test

---
+++ _lang/carbon/quick-start.sh
@@ -0,0 +1,27 @@
+# A variable with the nightly version from yesterday:
+VERSION="$(date -d yesterday +0.0.0-0.nightly.%Y.%m.%d)"
+
+# Get the release
+#wget https://github.com/carbon-language/carbon-lang/releases/download/v${VERSION}/carbon_toolchain-${VERSION}.tar.gz
+
+# Unpack the toolchain:
+tar -xvf carbon_toolchain-${VERSION}.tar.gz
+
+# Create a simple Carbon source file:
+echo "import Core library \"io\"; fn Run() { Core.Print(42); }" > forty_two.carbon
+
+# Compile to an object file:
+./carbon_toolchain-${VERSION}/bin/carbon compile \
+  --output=./bin/forty_two.o forty_two.carbon
+
+# Install minimal system libraries used for linking. Note that installing `gcc`
+# or `g++` for compiling C/C++ code with GCC will also be sufficient, these are
+# just the specific system libraries Carbon linking still uses.
+sudo apt install libgcc-11-dev
+
+# Link to an executable:
+./carbon_toolchain-${VERSION}/bin/carbon link \
+  --output=./bin/forty_two ./bin/forty_two.o
+
+# Run it:
+./bin/forty_two
